563dc9140841fda3eec5788065c703f9807bb58c
Shivam Jaiswal <shivam2001.jaisawal@gmail.com>
2026-09-24 09:46:37 +0530

    feat(umbraco): update startup configuration and service registration

--- UniPharGroup/Program.cs
+++ UniPharGroup/Program.cs
@@ -1,5 +1,11 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+
+if (builder.Environment.IsProduction())
+{
+    builder.WebHost.UseStaticWebAssets(); // Production-mode-from-source only — remove before publish/deploy
+}
+
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
@@ -10,7 +16,6 @@ builder.CreateUmbracoBuilder()
 WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
-
 
 app.UseUmbraco()
     .WithMiddleware(u =>
